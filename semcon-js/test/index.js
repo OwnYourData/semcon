92606a6a2d1c71eb46e3b50d6af230f009292f94
Christoph Fabianek <christoph.fabianek@gmail.com>
2023-05-06 14:02:30 +0200

    update semcon-js/test

--- semcon-js/test/index.js
+++ semcon-js/test/index.js
@@ -1,1 +1,15 @@
-const semcon = require('../dist/index.js');
+const { getSemcon } = require("semcon/dist/index.js");
+
+const baseUrl = "https://dec-support.data-container.net";
+const semcon = getSemcon(baseUrl);
+
+async function callVersion() {
+    try {
+        const versionData = await semcon.version();
+        console.log("Version: " + versionData["version"]);
+    } catch (error) {
+        console.error("Error fetching version:", error);
+    }
+}
+
+callVersion();
