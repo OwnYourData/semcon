ee6961f2129452741476d47f9d18c55e60dd0faf
Christoph Fabianek <christoph.fabianek@gmail.com>
2023-05-15 02:16:23 +0200

    simple wrappers

--- semcon-js/src/index.ts
+++ semcon-js/src/index.ts
@@ -1,4 +1,15 @@
-import axios from "axios";
+import axios, { AxiosResponse } from 'axios';
+
+interface user_PostData {
+    username: string;
+    password: string;
+}
+interface backup_PostData {
+    username: string;
+    password: string;
+    payload: any;
+    expiry?: Date;
+}
 
 export function getSemcon(
     baseUrl: string,
@@ -7,8 +18,61 @@ export function getSemcon(
         const retVal = await axios.get(`${baseUrl}/version`);
         return retVal.data;
     }
-    
+
+    async function checkUser(
+        username: string,
+        password: string
+    ): Promise<string> {
+        const data: user_PostData = {
+            username: username,
+            password: password,
+        };
+        const retVal: AxiosResponse = await axios.post(`${baseUrl}/checkUser`, data, {
+            headers: {'Content-Type': 'application/json'}
+        });
+        return retVal.data;
+    }
+
+    async function backup(
+        username: string,
+        password: string,
+        payload: any,
+        expiry?: Date
+    ): Promise<string> {
+        if (!expiry) {
+            let expiry: Date = new Date();
+            expiry.setFullYear(expiry.getFullYear() + 1);
+        }
+        const data: backup_PostData = {
+            username: username,
+            password: password,
+            payload: payload,
+            expiry: expiry,
+        };
+        const retVal: AxiosResponse = await axios.post(`${baseUrl}/backup`, data, {
+            headers: {'Content-Type': 'application/json'}
+        });
+        return retVal.data;
+    }
+
+    async function restore(
+        username: string,
+        password: string,
+    ): Promise<string> {
+        const data: user_PostData = {
+            username: username,
+            password: password,
+        };
+        const retVal: AxiosResponse = await axios.post(`${baseUrl}/restore`, data, {
+            headers: {'Content-Type': 'application/json'}
+        });
+        return retVal.data;
+    }
+
     return {
         version,
+        checkUser,
+        backup,
+        restore,
     }
 }
--- semcon-js/test/index.js
+++ semcon-js/test/index.js
@@ -1,6 +1,6 @@
-const { getSemcon } = require("semcon/dist/index.js");
+const { getSemcon } = require("../dist/index.js");
 
-const baseUrl = "https://dec-support.data-container.net";
+const baseUrl = "https://dec-backup.data-container.net";
 const semcon = getSemcon(baseUrl);
 
 async function callVersion() {
@@ -12,4 +12,41 @@ async function callVersion() {
     }
 }
 
+async function callCheckUser() {
+    try {
+        const checkUserData = await semcon.checkUser("john", "123");
+        console.log("checkUser Response: " + JSON.stringify(checkUserData, null, 2));
+
+    } catch (error) {
+        console.error("Error running checkUser:", error);
+    }
+}
+
+async function callBackup() {
+    try {
+        const backupData = await semcon.backup("john", "123",'{"hello":"TypeScript"}');
+        console.log("backup Response: " + JSON.stringify(backupData, null, 2));
+
+    } catch (error) {
+        console.error("Error running backup:", error);
+    }
+}
+
+async function callRestore() {
+    try {
+        const restoreData = await semcon.restore("john", "123");
+        console.log("restore Response: " + restoreData);
+
+    } catch (error) {
+        console.error("Error running restore:", error);
+    }
+}
+
+async function main() {
+    await callCheckUser();
+    await callBackup();
+    await callRestore();
+}
+
 callVersion();
+main();
